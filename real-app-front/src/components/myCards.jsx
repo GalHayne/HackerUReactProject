10c20ac6ae3858e13d5406cb15c97bbfc4aec0a0
Gal <galhayne@gmail.com>
2023-06-20 17:10:53 +0300

    fix render the favorite cards when click on edit card

--- real-app-front/src/components/myCards.jsx
+++ real-app-front/src/components/myCards.jsx
@@ -6,24 +6,24 @@ import { useEffect, useState } from "react";
 import axios from 'axios';
 import { useAuth } from "../context/auth.context";
 import { toast } from "react-toastify";
-import {  Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const MyCards = () => {
-  
-  const [onlyFavorite,setOnlyFavorite] = useState(false);
-  
-  const [favoriteCards,setFavoriteCards] = useState([]);
-  
+
+  const [onlyFavorite, setOnlyFavorite] = useState(false);
+
+  const [favoriteCards, setFavoriteCards] = useState([]);
+
   const { user } = useAuth();
 
   const cards = useMyCards();
-  
+
   useEffect(() => {
-      getFavoriteCards();
-  }, [])
+    getFavoriteCards();
+  }, [onlyFavorite])
 
   const getFavoriteCards = async () => {
-    if (user){
+    if (user) {
       try {
         const res = await axios.get(`http://localhost:3900/api/users/FavoriteCard/${user?._id}`)
         setFavoriteCards(res.data.favoriteCard);
@@ -32,27 +32,27 @@ const MyCards = () => {
       }
     }
   }
-    
-    const MoveTofavorite = async (card_id) => {
-      try {
-        await axios.put(`http://localhost:3900/api/users/addFavoriteCard/${card_id}/${user._id}`,{
-        })
-        toast.success(`The card move to favorite`)
-        getFavoriteCards();
-      
-    } catch (error) {   
+
+  const MoveTofavorite = async (card_id) => {
+    try {
+      await axios.put(`http://localhost:3900/api/users/addFavoriteCard/${card_id}/${user._id}`, {
+      })
+      toast.success(`The card move to favorite`)
+      getFavoriteCards();
+
+    } catch (error) {
       toast.error('server error cant move to favorite cards')
     }
   }
 
-    const removeFromfavorite = async (card_id) => {
-      try {
-        await axios.put(`http://localhost:3900/api/users/removeFavoriteCard/${card_id}/${user._id}`,{
-        })
-        toast.success(`The card remove from favorite`)
-        getFavoriteCards();
-      
-    } catch (error) {   
+  const removeFromfavorite = async (card_id) => {
+    try {
+      await axios.put(`http://localhost:3900/api/users/removeFavoriteCard/${card_id}/${user._id}`, {
+      })
+      toast.success(`The card remove from favorite`)
+      getFavoriteCards();
+
+    } catch (error) {
       toast.error('server error cant remove this card from favorite cards')
     }
   }
@@ -72,40 +72,42 @@ const MyCards = () => {
         <Link to="/create-card">Create a New Card</Link>
       </div>
       <div className="d-flex justify-content-end mb-3" >
-        {!onlyFavorite ? <button disabled={favoriteCards.length === 0} type="button" className="btn btn-primary w-25" onClick={() => setOnlyFavorite((prev) => !prev)}>Show only favorite cards </button> 
-        :
-        <button type="button" className="btn btn-primary w-25" onClick={() => setOnlyFavorite((prev) => !prev)}>Show all cards </button> 
+        {!onlyFavorite ? <button disabled={favoriteCards.length === 0} type="button" className="btn btn-primary w-25" onClick={() => setOnlyFavorite((prev) => !prev)}>Show only favorite cards </button>
+          :
+          <button type="button" className="btn btn-primary w-25" onClick={() => setOnlyFavorite((prev) => !prev)}>Show all cards </button>
         }
       </div>
-      
+
 
       <div className="d-flex justify-content-between m-3">
-        {!cards.length ? (<p>no cards...</p>) 
-        : onlyFavorite === false ? (
-          cards.map((card) => {
-            let isFavoriteCard = false;
-           favoriteCards.some(favoriteCard => {
-            if (favoriteCard._id === card._id){
-              isFavoriteCard = true;
-            }
-           })
-          
-            return <Card key={card._id} card={card} isFavoriteCard={isFavoriteCard} MoveTofavorite={MoveTofavorite} removeFromfavorite={removeFromfavorite}
-          />})
-        )
-        : (
-          favoriteCards.map((card) => {
-            let isFavoriteCard = false;
-           favoriteCards.some(favoriteCard => {
-            if (favoriteCard._id === card._id){
-              isFavoriteCard = true;
-            }
-           })
-          
-            return <Card key={card._id} card={card} isFavoriteCard={isFavoriteCard} MoveTofavorite={MoveTofavorite} removeFromfavorite={removeFromfavorite}
-          />})
-        )
-      }
+        {!cards.length ? (<p>no cards...</p>)
+          : onlyFavorite === false ? (
+            cards.map((card) => {
+              let isFavoriteCard = false;
+              favoriteCards.some(favoriteCard => {
+                if (favoriteCard._id === card._id) {
+                  isFavoriteCard = true;
+                }
+              })
+
+              return <Card key={card._id} card={card} isFavoriteCard={isFavoriteCard} MoveTofavorite={MoveTofavorite} removeFromfavorite={removeFromfavorite}
+              />
+            })
+          )
+            : (
+              favoriteCards.map((card) => {
+                let isFavoriteCard = false;
+                favoriteCards.some(favoriteCard => {
+                  if (favoriteCard._id === card._id) {
+                    isFavoriteCard = true;
+                  }
+                })
+
+                return <Card key={card._id} card={card} isFavoriteCard={isFavoriteCard} MoveTofavorite={MoveTofavorite} removeFromfavorite={removeFromfavorite}
+                />
+              })
+            )
+        }
       </div>
     </>
   );
