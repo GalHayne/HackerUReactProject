a1e3994014a33731ec8693760d29df31bddc9e8f
Gal <galhayne@gmail.com>
2023-06-23 19:54:02 +0300

    fix delete card

--- real-app-server/util/deleteCard.js
+++ real-app-server/util/deleteCard.js
@@ -2,15 +2,36 @@ const { Card } = require("../models/card");
 const { deleteAllUserThatFavorCrd } = require("../util/deleteAllUserThatFavorCrd")
 
 
-const deleteCard = async (card) => {
+const deleteCard = async (card_id) => {
+
+   const card = await Card.findOne({
+      _id: card_id,
+   });
 
    if (card) {
-      deleteAllUserThatFavorCrd(card)
+      const results = deleteAllUserThatFavorCrd(card)
+      results.then(async () => {
+
+         const deleteCard = await Card.findOneAndRemove({
+            _id: card_id,
+         });
+
+         if (!deleteCard) {
+            return false;
+            // return res.status(404).send("The card with the given ID was not found.");
+         }
+         return deleteCard;
+         // res.send(deleteCard);
+         // res.status(201);
+      })
+   } else {
+      return false;
+      // return res.status(404).send("The card with the given ID was not found.");
    }
 
-   return await Card.findOneAndRemove({
-      _id: card._id,
-   });
+
+
+
 
 
 }
